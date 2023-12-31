7a587a9d87ad5477bda463a52e38067134a2633a
Varkovi Mark <vmark33@gmail.com>
2023-12-31 03:34:18 +0100

    handle simple convos

--- client/chat.js
+++ client/chat.js
@@ -25,6 +25,7 @@ function addButton (element, id, textContent) {
 }
 
 function handleUserInput () {
+    let msgCounter = 0;
     const inputElement = document.querySelector('input')
     const submitBtnElement = document.querySelector('#submit-btn')
 
@@ -32,7 +33,7 @@ function handleUserInput () {
         const messageBoxElement = document.querySelector('.msg-box')
         messageBoxElement.style.display = "none"
         const messageFieldElement = document.querySelector('#msg-field')
-        inputElement.value.length > 0 ? messageFieldElement.insertAdjacentHTML('beforeend', `<div class="user-msg"><p>${inputElement.value}</p></div>`) : undefined
+        inputElement.value.length > 0 ? messageFieldElement.insertAdjacentHTML('beforeend', `<div class="user-msg"><p id="um">${inputElement.value}</p></div>`) : undefined
         
         inputElement.value = '';
         
@@ -42,19 +43,45 @@ function handleUserInput () {
     })
 }
 
+function addBotMessage (content) {
+    const messageFieldElement = document.querySelector('#msg-field')
+    return setTimeout(() => { messageFieldElement.insertAdjacentHTML('beforeend', `<div class="bot-msg"><p id="bm">${content}</p></div>`)}, 1200 )
+}
+
+async function greetUser () {
+    const answers = await fetchAnswers(answersURL)
+    const greetings = answers.greetings
+    const randomGreeting = Math.round(Math.random() * greetings.length)
+    return addBotMessage(greetings[randomGreeting])
+}
+
+function handleUserOrder () {
+
+}
 
 async function generateAnswers () {
-    const answers = await fetchAnswers(answersURL)
-    console.log(answers);
-    const inputElement = document.querySelector('input')
-    const messageFieldElement = document.querySelector('#msg-field')
+    let userMsgCounter = 0;
     const submitBtnElement = document.querySelector('#submit-btn')
     submitBtnElement.addEventListener('click', () => {
-        const greetings = answers.greetings
-        const randomNumber = Math.round(Math.random() * greetings.length)
-    setTimeout(() => {
-        messageFieldElement.insertAdjacentHTML('beforeend', `<div class="bot-msg"><p>${greetings[randomNumber]}</p></div>`)
-    }, 1200 )
+        const allUserMessages = document.querySelectorAll('#um')
+        const lastUserMessage = allUserMessages[allUserMessages.length -1].textContent
+
+        userMsgCounter++
+    userMsgCounter < 2 && !lastUserMessage.toLowerCase().includes("order")  ? greetUser() : undefined;
+    
+    if (userMsgCounter > 1)  { 
+    const allBotMessages = document.querySelectorAll('#bm')
+    const lastBotMessage = allBotMessages[allBotMessages.length -1].textContent
+
+    if (lastUserMessage.toLowerCase().includes("order")) { 
+        addBotMessage('Please provide your order number')
+    }
+    if (/^[0-9]+$/.test(lastUserMessage)) {
+        addBotMessage('Thank you, it looks like your order is on its way! Is there anything else I can help you with?')
+    } else if (!/^[0-9]+$/.test(lastUserMessage) && lastBotMessage.includes("provide")) {
+        addBotMessage("Please provide a correct order number")
+    }
+}
     })
 }
 
